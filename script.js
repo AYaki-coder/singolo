0e9891266c1024a7b186ffb891466f80d8b51b1a
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-03-17 01:04:56 +0300

    feat: add switching tags& border to pics

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 window.onload = function () {
     console.log('Hi,Lena!');
     addLinksClickHandler();
+    addTagsClickHandler();
+    addGalleryClickHandler();
 }
 const addLinksClickHandler = () => {
 
@@ -29,7 +31,57 @@ const selectClickedLink = (clickedLink) => {
 }
 //Slider
 //Portfolio
-//Get a Quote
+ 
+
+const addTagsClickHandler = () => {
+
+    document.querySelector('.portfolio__tag-box').addEventListener('click', (e) => {
+        if (e.target.classList.contains('portfolio__tag')) {
+            let clickedTag = e.target;
+            removeSelectedTag();
+            selectClickedTag(clickedTag);
+            //прицепить перетассовку картинок
+        }
+    })
+}
+
+const removeSelectedTag = () => {
+    let tags = document.querySelectorAll('.portfolio__tag-box .portfolio__tag');
+    tags.forEach( (tag) => {
+        tag.classList.remove('tag_selected');
+    })
+}
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+}
+
+const addGalleryClickHandler = () => {
+
+    document.querySelector('.portfolio__album').addEventListener('click', (e) =>{
+    if (e.target.classList.contains('portfolio__pics')) {
+        let clickedPic = e.target;
+        removeSelectedPic();
+        selectClickedPic(clickedPic);
+    }
+ })
+}
+
+const  removeSelectedPic = () => {
+
+    let pics = document.querySelectorAll('.portfolio__album .portfolio__pics');
+    console.log(pics);
+    pics.forEach( (pic) => {
+      pic.classList.remove('pic_selected');
+
+    })
+
+}
+
+const  selectClickedPic = (clickedPic) => {
+
+    clickedPic.classList.add('pic_selected');
+}
+ //Get a Quote
 
 
 
